0a318d409eaa29775186df6f5f0683f2bb8d48dd
katsiev1997 <katsiev1997@gmail.com>
2024-05-05 23:57:37 +0300

    class Command and command Start

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,21 @@ import { Telegraf, session } from "telegraf";
 import { ConfigService } from "../config.service";
 import { IConfigService } from "./config/config.interface";
 import { IBotContext } from "./context/context.interface";
+import { Command } from "./commands/command.class";
+import { StartCommand } from "./commands/start.command";
 
 class Bot {
   bot: Telegraf<IBotContext>;
+  commands: Command[] = [];
   constructor(private readonly configService: IConfigService) {
     this.bot = new Telegraf<IBotContext>(this.configService.get("TOKEN"));
     this.bot.use(session());
   }
   init() {
+    this.commands = [new StartCommand(this.bot)];
+    for (const command of this.commands) {
+      command.handle();
+    }
     this.bot.launch();
   }
 }
---
+++ src/commands/start.command.ts
@@ -0,0 +1,30 @@
+import { Markup, Telegraf } from "telegraf";
+import { IBotContext } from "../context/context.interface";
+import { Command } from "./command.class";
+
+export class StartCommand extends Command {
+  constructor(bot: Telegraf<IBotContext>) {
+    super(bot);
+  }
+
+  handle(): void {
+    this.bot.start((ctx) => {
+      console.log(ctx.session);
+      ctx.reply(
+        "Вам понравился курс?",
+        Markup.inlineKeyboard([
+          Markup.button.callback("Да", "course_like"),
+          Markup.button.callback("Нет", "course_dislike"),
+        ])
+      );
+    });
+    this.bot.action("course_like", (ctx) => {
+        ctx.session.courseLike = true;
+        ctx.editMessageText("Круто")
+    })
+    this.bot.action("course_dislike", (ctx) => {
+        ctx.session.courseLike = false;
+        ctx.editMessageText("Как же так.")
+    })
+  }
+}
