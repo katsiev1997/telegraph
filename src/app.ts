0d9993edb1642b7da54c4ccebeff41b8acc228eb
katsiev1997 <katsiev1997@gmail.com>
2024-05-06 00:00:58 +0300

    Локальное хранение сессий

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
-import { Telegraf, session } from "telegraf";
+import { Telegraf } from "telegraf";
+import LocalSession from "telegraf-session-local";
 import { ConfigService } from "../config.service";
-import { IConfigService } from "./config/config.interface";
-import { IBotContext } from "./context/context.interface";
 import { Command } from "./commands/command.class";
 import { StartCommand } from "./commands/start.command";
+import { IConfigService } from "./config/config.interface";
+import { IBotContext } from "./context/context.interface";
 
 class Bot {
   bot: Telegraf<IBotContext>;
   commands: Command[] = [];
   constructor(private readonly configService: IConfigService) {
     this.bot = new Telegraf<IBotContext>(this.configService.get("TOKEN"));
-    this.bot.use(session());
+    this.bot.use(new LocalSession({ database: "session.json" }).middleware());
   }
   init() {
     this.commands = [new StartCommand(this.bot)];
